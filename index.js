4f3be8bddc666c24d2fd0a0d7d53cd0ebe2dc805
Janne Hietamäki <janne.hietamaki@iki.fi>
2020-08-17 21:57:54 +0300

    breaks tests etc, but exports ecr login + password

--- index.js
+++ index.js
@@ -40,6 +40,8 @@ async function run() {
       // Execute the docker login command
       let doLoginStdout = '';
       let doLoginStderr = '';
+      core.setOutput('user', creds[0]);
+      core.setOutput('password', creds[1]);
       const exitCode = await exec.exec('docker login', ['-u', creds[0], '-p', creds[1], proxyEndpoint], {
         silent: true,
         ignoreReturnCode: true,
